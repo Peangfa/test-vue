7a57660eaecd5626537dfc998404e54b4f04657b
Tongnisachol <tongsuksawat33@gmail.com>
2023-03-20 15:05:21 +0700

    tong

--- src/components/Checkin.vue
+++ src/components/Checkin.vue
@@ -1,14 +1,172 @@
 <template >
-        <h4 style="margin-top: 30px;margin-left: 40px;">การเข้างาน</h4>
-    
+  <h2 style="margin-top: 30px; margin-left: 40px">การเข้างาน</h2>
+  <v-container>
+    <v-row>
+      <v-col sm="12" md="6" style="display: flex; justify-content: center">
+        <div>
+          <v-card
+            height=" 450px "
+            width=" 450px "
+            elevation="5"
+            style="margin-top: 100px; padding: 18px; border-radius: 16px"
+          >
+            <v-avatar
+              size="260"
+              style="margin-top: 25px; margin-left: 70px; margin-right: 70px"
+            >
+              <v-img
+                aspect-ratio="1/1"
+                cover
+                src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"
+              ></v-img>
+            </v-avatar>
+            <p class="text-center text-h6 mb-1" style="padding-top: 50px">สุชาดา ศิริโกศล</p>
+            <p class="text-center text-h6 mb-1" style="padding-top;: 5px">Human Resouces</p>
+          </v-card>
+
+          <v-row justify="center">
+            <v-col cols="auto" style="display: flex; padding-top: 70px">
+              <v-dialog activator="parent" width="auto">
+                <template v-slot:activator="{ props }">
+                  <v-btn color="success" size="x-large" v-bind="props"
+                    >บันทึกเข้า</v-btn
+                  >
+                </template>
+                <template v-slot:default="{ isActive }">
+                  <v-card>
+                    <v-toolbar color="success" title="บันทึกการออกงาน">
+                    </v-toolbar>
+                    <v-card-text>
+                      <div class="text-h5 pa-12">
+                        บันทึกเวลาเข้างานของคุณเรียบร้อยแล้ว
+                      </div>
+                    </v-card-text>
+                    <v-card-actions class="justify-center">
+                      <v-btnd
+                        rounded="pill"
+                        variant="tonal"
+                        size="x-large"
+                        @click="isActive.value = false"
+                        >ปิด</v-btnd
+                      >
+                    </v-card-actions>
+                  </v-card>
+                </template>
+              </v-dialog>
+
+              <v-dialog activator="parent" width="auto">
+                <template v-slot:activator="{ props }">
+                  <v-btn
+                    color="error"
+                    size="x-large"
+                    v-bind="props"
+                    style="margin-left: 50px"
+                    >บันทึกออก</v-btn
+                  >
+                </template>
+                <template v-slot:default="{ isActive }">
+                  <v-card>
+                    <v-toolbar color="error" title="บันทึกการออกงาน">
+                    </v-toolbar>
+                    <v-card-text>
+                      <div class="text-h5 pa-12">
+                        บันทึกเวลาออกงานของคุณเรียบร้อยแล้ว
+                      </div>
+                    </v-card-text>
+                    <v-card-actions class="justify-center">
+                      <v-btnd
+                        rounded="pill"
+                        variant="tonal"
+                        size="x-large"
+                        @click="isActive.value = false"
+                        >ปิด</v-btnd
+                      >
+                    </v-card-actions>
+                  </v-card>
+                </template>
+              </v-dialog>
+            </v-col>
+          </v-row>
+        </div>
+      </v-col>
+
+      <v-col sm="12" md="6">
+        <div>
+          <h3 style="margin-top: 30px; margin-left: 40px">ประวัติการเข้างาน</h3>
+          <br />
+          <v-card>
+            <v-table>
+              <thead style="background-color: #e0e0e0">
+                <tr>
+                  <th class="text-center">วันที่</th>
+                  <th class="text-center">เวลา</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in desserts" :key="item.date">
+                  <td class="text-center">{{ item.date }}</td>
+                  <td class="text-center">{{ item.time }}</td>
+                </tr>
+              </tbody>
+            </v-table>
+          </v-card>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 
 <script>
 export default {
-    name:'checkin',
-}
+  name: "checkin",
+  data() {
+    return {
+      desserts: [
+        {
+          date: "22  มกราคม 2566",
+          time: "08.55 น.",
+        },
+        {
+          date: "23  มกราคม 2566",
+          time: "08.49น.",
+        },
+        {
+          date: "24  มกราคม 2566",
+          time: "09.05 น.",
+        },
+        {
+          date: "25  มกราคม 2566",
+          time: "08.59 น.",
+        },
+        {
+          date: "26  มกราคม 2566",
+          time: "08.55 น.",
+        },
+        {
+          date: "27  มกราคม 2566",
+          time: "08.45 น.",
+        },
+        {
+          date: "30  มกราคม 2566",
+          time: "08.51 น.",
+        },
+        {
+          date: "31  มกราคม 2566",
+          time: "08.55 น.",
+        },
+        {
+          date: "1 กุมภาพันธ์ 2566",
+          time: "08.55 น.",
+        },
+        {
+          date: "2 กุมภาพันธ์ 2566",
+          time: "08.55 น.",
+        },
+      ],
+    };
+  },
+};
 </script>
 <style>
-    
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,185 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <h4 style="margin-top: 30px;margin-left: 40px;">หน้าหลัก</h4>
+  <h2 style="padding-left: 20px; padding-top: 20px; color: black">หน้าหลัก</h2>
+  <v-container style="padding: 20px">
+    <v-row>
+      <v-col sm="12" md="6">
+        <v-card
+          style="padding-left: 15px; margin-top: 40px; border-radius: 16px"
+          elevation="2"
+          max-width="600"
+          rounded="16"
+          border-radius="16px"
+          class="pa-4 mx-auto"
+        >
+          <v-list-item-title class="text-h5 mb-1">
+            ยินดีต้อนรับ คุณสุชาดา ศิริโกศล
+          </v-list-item-title>
+          <br>
+          <p class="text-left  text-h6 mb-1">
+         ขอให้มีความสุขกับการทำงานในวันนี้ !
+    </p>
+          <p class="text-left  text-h8 mb-1">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </p>
+          <v-col sm="12" md="4" >
+            <v-img src="../assets/Working.png" alt="Working"></v-img>
+          </v-col>
+        </v-card>
+      </v-col>
 
+      <v-col sm="12" md="6">
+        <v-img src="../assets/Hello.png" alt="Hello"></v-img>
+      </v-col>
+    </v-row>
 
+    <v-row>
+      <v-col sm="12" md="6">
+        <div style="padding-left: 15px; margin-top: 30px">
+          <v-btn size="large"  color="#FDE10A" rounded="pill">ข่าวสารและอีเวนต์ต่างๆ</v-btn>
+        </div>
+        <br />
 
-      
+        <v-card-text>
+          <div class="text-body-1">
+            <v-icon>mdi-calendar-month</v-icon>วันที่ 5 มกราคม 2566
+          </div>
+          <div class="text-body-1 text--primary">
+            ซินเจียยู่อี่ ซินนี้ฮวดไช้ ตรุษจีนปีนี้ อิ่มปังรับอั่งเปา
+            เพียงเปิดแอป WESERVE เมื่อสั่งอาหารทุก ฿500 ขึ้นไป รับอั่งเปากินฟรี!
+            ฿880 อยากเฮง ลุ้นกินฟรีแบบนี้ ไม่ควรพลาด!
+          </div>
+        </v-card-text>
+
+        <v-card-text>
+          <div class="text-body-1">
+            <v-icon>mdi-calendar-month</v-icon>วันที่ 14 กุมภาพันธ์ 2566
+          </div>
+          <div class="text-body-1 text--primary">
+            ขอเชิญทุกคนมาร่วมฉลองความรักอันไร้ขอบเขตในเดือนแห่งความรักนี้ กับงาน
+            ‘Love is Love : Love has no boundaries’
+            ที่มาพร้อมกับหลากหลายกิจกรรมส่งต่อความรักในทุกรูปแบบ
+          </div>
+        </v-card-text>
+
+        <v-card-text>
+          <div class="text-body-1">
+            <v-icon>mdi-calendar-month</v-icon>วันที่ 5 มีนาคม 2566
+          </div>
+          <div class="text-body-1 text--primary">
+            ขอเชิญเข้าร่วมทำบุญทอดกฐิน ณ วัดฉลอง
+          </div>
+        </v-card-text>
+
+        <v-card-text>
+          <div class="text-body-1">
+            <v-icon>mdi-calendar-month</v-icon>วันที่ 14 มีนาคม 2566
+          </div>
+          <div class="text-body-1 text--primary">
+            OPEN HOUSE
+            ขอเชิญทุกคนมาร่วมฉลองความรักอันไร้ขอบเขตในเดือนแห่งความรักนี้กับงาน
+            ‘Love is Love : Love has no boundaries’
+            ที่มาพร้อมกับหลากหลายกิจกรรมส่งต่อความรักในทุกรูปแบบ
+          </div>
+        </v-card-text>
+      </v-col>
+     
+
+      <v-col sm="12" md="6">
+        <v-card
+          elevation="3"
+          style="padding-left: 15px; margin-top: 40px; border-radius: 16px"
+        >
+          <v-card-title >กิจกรรมของวันนี้</v-card-title>
+
+          <v-timeline side="end" align="start">
+            <v-timeline-item dot-color="green" size="small">
+              <div class="d-flex">
+                <strong class="me-4"
+                  >09.30 น.
+                  <div class=""></div
+                ></strong>
+                <div>
+                  <strong>Scrum meeting</strong>
+                </div>
+              </div>
+            </v-timeline-item>
+            <v-timeline-item dot-color="green" size="small">
+              <div class="d-flex">
+                <strong class="me-4"
+                  >10.00 น.
+                  <div class=""></div
+                ></strong>
+                <div>
+                  <strong>Scrum meeting</strong>
+                </div>
+              </div>
+            </v-timeline-item>
+            <v-timeline-item dot-color="green" size="small">
+              <div class="d-flex">
+                <strong class="me-4"
+                  >11.00 น.
+                  <div class=""></div
+                ></strong>
+                <div>
+                  <strong>Scrum meeting</strong>
+                </div>
+              </div>
+            </v-timeline-item>
+
+            <v-timeline-item dot-color="green" size="small">
+              <div class="d-flex">
+                <strong class="me-4">14.00 น.</strong>
+                <div>
+                  <strong>ประชุมกับลูกค้า</strong>
+                </div>
+              </div>
+            </v-timeline-item>
+
+            <v-timeline-item dot-color="green" size="small">
+              <div class="d-flex">
+                <strong class="me-4">15.00 น.</strong>
+                <div>
+                  <strong>ซื้อของเข้าออฟฟิศ</strong>
+                </div>
+              </div>
+            </v-timeline-item>
+
+            <v-timeline-item dot-color="green" size="small">
+              <div class="d-flex">
+                <strong class="me-4">17.30 น.</strong>
+                <div>
+                  <strong>เล่นเกม</strong>
+                </div>
+              </div>
+            </v-timeline-item>
+            <v-timeline-item dot-color="green" size="small">
+              <div class="d-flex">
+                <strong class="me-4">18.00 น.</strong>
+                <div>
+                  <strong>เคลียร์งาน</strong>
+                </div>
+              </div>
+            </v-timeline-item>
+
+            <v-timeline-item dot-color="green" size="small">
+              <div class="d-flex">
+                <strong class="me-4">19.30 น.</strong>
+                <div>
+                  <strong>ทำ OT</strong>
+                </div>
+              </div>
+            </v-timeline-item>
+          </v-timeline>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
 
-  data: () => ({
-  }),
-}
+  data: () => ({}),
+};
 </script>
